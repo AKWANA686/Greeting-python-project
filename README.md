ad4358c0bfb38f8097d5f790ddd162f8ee9aae3b
Akwana686 <akwanaoliver@gmail.com>
2025-03-12 12:59:35 +0300

    A simple project on creating a personalized greeting app

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Greeting-python-project
+
+This is a python weekly coding exercise for POWER LEARN PROJECT on creating a program that asks for the user’s name and favorite color, then prints a personalized greeting like: “Hello, [Name]! Your favorite color, [Color], is awesome!”
+
+The solution is provided in the greeting.py file.
---
+++ greeting.py
@@ -0,0 +1,7 @@
+# 1. Personalized Greeting App
+
+# Create a program that asks for the user’s name and favorite color, then prints a personalized greeting like: “Hello, [Name]! Your favorite color, [Color], is awesome!”
+
+name = "Akwana"
+color = "black"
+print("Hello " + name + "! Your favourite color, " + color + ", is awesome.")
